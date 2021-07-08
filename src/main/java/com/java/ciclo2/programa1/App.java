6066a4af73d768dc0c6188e841dd6c2230a754e8
Cerf <cerf_fx@yahoo.com>
2021-07-08 09:43:29 -0500

    commit A1

---
+++ src/main/java/com/java/ciclo2/programa1/App.java
@@ -0,0 +1,12 @@
+package com.java.ciclo2.programa1;
+
+/*Primer programa mio en java
+veamos como me va -------------
+*/
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
